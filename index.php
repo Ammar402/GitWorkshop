0ad3109ef183db96e284f165a304ded6e8ab3343
ammar ramly <nizzix_402@yahoo.com>
2017-10-21 16:24:44 +0800

    index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 212px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #333;
+   color: white;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="index.php"class="active" href="#home">Home</a></li>
+  <li><a href="login.php">Login</a></li>
+  <li><a href="register.php">Register</a></li>
+  <li><a href="contact_us.php">Contact Us</a></li>
+</ul>
+<div class="footer">
+  <p>WWE Main Page</p>
+</div>
+
+
+</body>
+</html>
